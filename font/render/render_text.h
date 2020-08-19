4d094d334b80a68604de9fb317494007fd9488f4
Thomas Deniffel <tdeniffel@acm.org>
2020-08-19 10:07:55 +0200

    Loading of freetype works

--- font/render/render_text.h
+++ font/render/render_text.h
@@ -3,8 +3,20 @@
 
 #include <string>
 #include <iostream>
+#include <freetype2/ft2build.h>
+#include FT_FREETYPE_H
 
 void render_text(std::string text) {
+
+    FT_Library ft; 
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init Freetype Library" << std::endl;
+        // TODO: error handling
+        return;      
+    }
+
+
     std::cout << text << std::endl;
 }
 
